255c6bdfa9b0ad503f767f70a28e02bb25ed8d85
Tygo Egmond <tygo.egmond@student.rocva.nl>
2020-03-23 22:02:25 +0100

    [Server] Get logged in User

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// Middleware
+const auth = require('../middleware/auth');
+
+// Models
+const User = require('../models/User');
+
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        res.json(user);
+    } catch (err) {
+        console.error(`\x1b[41m\x1b[30m[ERROR]\x1b[0m \x1b[34m[Route: GET - api/auth] \x1b[31m${err.message}\x1b[0m`);
+        res.status(500).json({ msg: 'Something went wrong on our side.' });
+    }
 });
 
 // @route   POST api/auth
