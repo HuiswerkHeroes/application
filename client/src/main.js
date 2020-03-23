9690b0622d45a42c29e1184ace1da04eb502eb82
Tygo Egmond <tygo.egmond@student.rocva.nl>
2020-03-23 11:48:16 +0100

    [Client] Installed Latte-UI v1.8.3

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import LatteUI from '@bybas/latte-ui';
+
+Vue.use(LatteUI);
+
+Vue.config.productionTip = false;
+
+import '@bybas/latte-ui/src/scss/app.scss';
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
