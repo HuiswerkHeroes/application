55535b1cc1b7437e0e0c56f17445f3f432045141
Tygo Egmond <tygo.egmond@student.rocva.nl>
2020-03-23 15:45:06 +0100

    [Server] Backend Routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Welkom bij de Huiswerk Heroes API' }));
 
+// Define Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
