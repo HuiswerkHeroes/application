06681ce399a9fb7da57f793c498d829540d8b222
Tygo Egmond <tygo.egmond@student.rocva.nl>
2020-04-02 17:14:00 +0200

    [Server] Model aangemaakt voor school_hoofdvestigingen

---
+++ server/models/SchoolHoofdvestiging.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const SchoolHoofdvestigingSchema = mongoose.Schema({
+    BRIN: {
+        type: String,
+        required: true,
+        uppercase: true,
+        unique: true,
+        max: 4
+    },
+    gemeente: {
+        type: String,
+        required: true,
+        uppercase: true
+    },
+    naam: {
+        type: String,
+        required: true
+    },
+    actief: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
+module.exports = mongoose.model('SchoolHoofdvestiging', SchoolHoofdvestigingSchema, { collection: 'school_hoofdvestigingen' });
