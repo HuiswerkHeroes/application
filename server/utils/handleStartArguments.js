489349c59b5fe9fd409357f980ac3d107bfcaaad
Tygo Egmond <tygo.egmond@icloud.com>
2020-04-08 10:47:44 +0200

    [Server] Database Seeder afgemaakt

--- server/utils/handleStartArguments.js
+++ server/utils/handleStartArguments.js
@@ -2,45 +2,45 @@
  * Copyright (c) 2020
  * Dit project is gemaakt door Tygo Egmond (tygoegmond.nl)
  */
-
-// const mongoose = require('mongoose');
-
-// Require Models
-const Rol = require('../models/Rol');
-const GebruikerType = require('../models/GebruikerType');
-
-// require('fs')
-//     .readdirSync('./models')
-//     .forEach(function (file) {
-//         require('../models/' + file);
-//     });
+const fs = require('fs')
 
 /**
- *  @util   handleStartArguments
+ *  @func   handleStartArguments (exports)
  *  @desc   Handle NodeJS Start Arguments
  */
 const handleStartArguments = () => {
     process.argv.forEach(async function (val, index, array) {
         if (val === '--db-seed') {
-            console.log('\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mStarting to seed the database');
             await seedDatabase();
         }
     });
 };
 
+/**
+ *  @func   seedDatabase
+ *  @desc   Seed de database
+ */
 const seedDatabase = async () => {
-    const fs = require('fs');
-    let rawdata = await JSON.parse(fs.readFileSync(__dirname + '/database/seeder.json'));
-    rawdata.forEach((element) => {
-        element.rijen.forEach((element) => {
-            Rol.create(element, (err) => {
-                console.error(err);
-            });
-        });
-    });
+    const rawSeedData = JSON.parse(fs.readFileSync(__dirname +  '/database/seeder.json'))
+    console.log('\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mStarting to seed the database');
+
+    // Voor elke collection
+    rawSeedData.forEach((element) => {
+        const Model = require('../models/' + element.modelNaam);
+        console.log(`\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mSeeding model: ${element.modelNaam}`);
+
+        // Voor elke rij
+        element.rijen.forEach(async (element) => {
+            await Model.findOneAndUpdate(element, element, {upsert: true, new: true, runValidators: true, useFindAndModify: false}).catch((err) => {
+                console.error(err)
+            }); 	
+        })
+
+        console.log(`\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mSeeding model: ${element.modelNaam} - Finished`);
+    })
 
     console.log('\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mSeeding completed, stopping application.');
     process.exit(1);
-};
+}
 
 module.exports = handleStartArguments;
