c258bd331710c8ab7d616d90a32a994729da9bca
Tygo Egmond <tygo.egmond@icloud.com>
2020-04-08 20:30:06 +0200

    [Server] Veranderd dat de seeder niet de server stopt

--- server/utils/handleStartArguments.js
+++ server/utils/handleStartArguments.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2020
  * Dit project is gemaakt door Tygo Egmond (tygoegmond.nl)
  */
-const fs = require('fs')
+const fs = require('fs');
 
 /**
  *  @func   handleStartArguments (exports)
@@ -21,7 +21,7 @@ const handleStartArguments = () => {
  *  @desc   Seed de database
  */
 const seedDatabase = async () => {
-    const rawSeedData = JSON.parse(fs.readFileSync(__dirname +  '/database/seeder.json'))
+    const rawSeedData = JSON.parse(fs.readFileSync(__dirname + '/database/seeder.json'));
     console.log('\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mStarting to seed the database');
 
     // Voor elke collection
@@ -31,16 +31,17 @@ const seedDatabase = async () => {
 
         // Voor elke rij
         element.rijen.forEach(async (element) => {
-            await Model.findOneAndUpdate(element, element, {upsert: true, new: true, runValidators: true, useFindAndModify: false}).catch((err) => {
-                console.error(err)
-            }); 	
-        })
+            await Model.findOneAndUpdate(element, element, { upsert: true, new: true, runValidators: true, useFindAndModify: false }).catch((err) => {
+                console.error(err);
+            });
+        });
 
         console.log(`\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mSeeding model: ${element.modelNaam} - Finished`);
-    })
+    });
 
-    console.log('\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mSeeding completed, stopping application.');
-    process.exit(1);
-}
+    console.log(
+        '\x1b[47m\x1b[30m[LOG]\x1b[0m \x1b[34m[Database Seeder] \x1b[0mSeeding completed, please wait just a moment for the seeder to complete.'
+    );
+};
 
 module.exports = handleStartArguments;
