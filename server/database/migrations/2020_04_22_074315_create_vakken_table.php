1bcadbd468c5829b3149bbeb945ea2fc78829564
Tygo Egmond <tygo.egmond@icloud.com>
2020-04-22 10:30:40 +0200

    [Server] Timestamps toegevoegd aan migration: vakken

--- server/database/migrations/2020_04_22_074315_create_vakken_table.php
+++ server/database/migrations/2020_04_22_074315_create_vakken_table.php
@@ -16,6 +16,7 @@ class CreateVakkenTable extends Migration
         Schema::create('vakken', function (Blueprint $table) {
             $table->id();
             $table->string('naam')->unique();
+            $table->timestamps();
         });
     }
 
