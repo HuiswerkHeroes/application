15816e139775d1d6e45832a5b5a7af361ae4a29d
Tygo Egmond <tygo.egmond@icloud.com>
2020-04-22 09:41:59 +0200

    [Server] Een model/migration voor school_locaties

---
+++ server/app/SchoolLocatie.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright (c) 2020.
+ * Dit bestand is aangemaakt door Tygo Egmond (tygoegmond.nl)
+ */
+
+namespace App;
+
+
+class SchoolLocatie extends \Illuminate\Database\Eloquent\Model
+{
+    /**
+     * Tabel naam
+     *
+     * @var string
+     */
+    protected $table = 'school_locaties';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'vestigingsnummer', 'hoofdvestiging_id', 'naam', 'plaatsnaam', 'adres', 'actief'
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'actief' => 'boolean',
+    ];
+
+    /**
+     * @param $value
+     * @noinspection PhpUnused
+     */
+    public function setVestigingsnummerAttribute($value)
+    {
+        $this->attributes['vestigingsnummer'] = strtoupper($value);
+    }
+
+    /**
+     * @param $value
+     * @noinspection PhpUnused
+     */
+    public function setPlaatsnaamAttribute($value)
+    {
+        $this->attributes['plaatsnaam'] = strtoupper($value);
+    }
+
+    /**
+     * @param $value
+     * @noinspection PhpUnused
+     */
+    public function setAdresAttribute($value)
+    {
+        $this->attributes['adres'] = strtoupper($value);
+    }
+}
