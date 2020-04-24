dbaa19e719d9652914802ccca0376779528f3926
Tygo Egmond <tygo.egmond@student.rocva.nl>
2020-04-24 16:58:26 +0200

    [Client] Begin aan de sidebar

--- client/src/views/v1/Skeleton.vue
+++ client/src/views/v1/Skeleton.vue
@@ -7,33 +7,42 @@
     <div>
         <header>
             <div class="app-bar app-bar-primary">
-                <div class="container">
-                    <div class="app-bar-row">
-                        <router-link class="app-bar-title nav-link" :to="{ name: 'Dashboard' }">HuiswerkHeroes</router-link>
+                <div class="app-bar-row">
+                    <router-link class="app-bar-title nav-link" :to="{ name: 'Dashboard' }">HuiswerkHeroes</router-link>
 
-                        <div class="ml-auto">
-                            <strong class="mx-2" style="font-size: 0.9em;" v-if="getGebruiker.setup_afgerond">{{ getGebruiker.opleiding.naam }} - {{ getGebruiker.school_locatie.naam }}</strong>
-                            <button class="btn btn-text btn-icon  btn-lg" ref="userDropdown">
-                                <latte-initials :initials="getGebruiker.initialen" class="avatar"></latte-initials>
-                            </button>
-                        </div>
-                        <latte-popup :associate-with="$refs.userDropdown" :margin-x="-9">
-                            <nav class="nav nav-list">
-                                <router-link class="nav-link" :to="{ name: 'Instellingen' }">Instellingen</router-link>
-
-                                <div class="divider divider-horizontal"></div>
-
-                                <form v-on:submit="handleSignOut">
-                                    <button class="nav-link" type="submit">
-                                        <span>Uitloggen</span>
-                                    </button>
-                                </form>
-                            </nav>
-                        </latte-popup>
+                    <div class="ml-auto">
+                        <strong class="mx-2" style="font-size: 0.9em;" v-if="getGebruiker.setup_afgerond">{{ getGebruiker.opleiding.naam }} - {{ getGebruiker.school_locatie.naam }}</strong>
+                        <button class="btn btn-text btn-icon  btn-lg" ref="userDropdown">
+                            <latte-initials :initials="getGebruiker.initialen" class="avatar"></latte-initials>
+                        </button>
                     </div>
+                    <latte-popup :associate-with="$refs.userDropdown" :margin-x="-9">
+                        <nav class="nav nav-list">
+                            <router-link class="nav-link" :to="{ name: 'Instellingen' }">Instellingen</router-link>
+
+                            <div class="divider divider-horizontal"></div>
+
+                            <form v-on:submit="handleSignOut">
+                                <button class="nav-link" type="submit">
+                                    <span>Uitloggen</span>
+                                </button>
+                            </form>
+                        </nav>
+                    </latte-popup>
                 </div>
             </div>
         </header>
+
+        <aside id="sidebar">
+            <latte-bottom-nav is-side class="bottom-nav-dark">
+                <latte-ripple as="a" class="btn btn-action"><i class="mdi mdi-home"></i><span>Home</span></latte-ripple>
+                <latte-ripple as="a" class="btn btn-action"><i class="mdi mdi-compass"></i><span>Discover</span></latte-ripple>
+                <latte-ripple as="a" class="btn btn-action"><i class="mdi mdi-music-note"></i><span>My music</span></latte-ripple>
+                <latte-ripple as="a" class="btn btn-action"><i class="mdi mdi-radio-tower"></i><span>Radio</span></latte-ripple>
+                <latte-ripple as="a" class="btn btn-action"><i class="mdi mdi-shopping-music"></i><span>Store</span></latte-ripple>
+            </latte-bottom-nav>
+        </aside>
+
         <main>
             <router-view />
         </main>
