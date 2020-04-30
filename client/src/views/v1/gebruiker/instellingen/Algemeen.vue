c5e903c8380569955908939cb89ac6ef43c4b842
Tygo Egmond <tygo.egmond@icloud.com>
2020-05-01 00:43:56 +0200

    [Client] Begonnen aan de algemene instellingen

--- client/src/views/v1/gebruiker/instellingen/Algemeen.vue
+++ client/src/views/v1/gebruiker/instellingen/Algemeen.vue
@@ -5,6 +5,52 @@
 
 <template>
     <div>
-        <h1>...</h1>
+        <div class="panel">
+            <form>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label for="voornaam">Voornaam</label>
+                        <input id="voornaam" type="text" class="form-control" v-model="voornaam" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="achternaam">Achternaam</label>
+                        <input id="achternaam" type="text" class="form-control" v-model="achternaam" required />
+                    </div>
+                </div>
+
+                <div class="panel-footer">
+                    <button type="submit" class="btn btn-contained btn-primary ml-auto" v-bind:disabled="this.laden">
+                        <span v-if="this.laden" class="spinner"></span>
+                        <span>Opslaan</span>
+                    </button>
+                </div>
+            </form>
+            <div class="loading-overlay">
+                <span class="spinner"></span>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    // import Header from '@/components/layout/Header';
+
+    export default {
+        name: 'Instellingen',
+        data() {
+            return {
+                laden: false,
+                voornaam: '',
+                achternaam: '',
+                success: ''
+            };
+        },
+        components: {
+        },
+        computed: mapGetters('auth', ['getGebruiker']),
+        mounted() {
+            this.voornaam = this.getGebruiker.voornaam;
+        }
+    };
+</script>
