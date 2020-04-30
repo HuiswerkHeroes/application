6cf22be2d3a1189ebc51ca227cc4fc34083c906f
Tygo Egmond <tygo.egmond@icloud.com>
2020-05-01 00:40:34 +0200

    [Client] Offline functionaliteit toegevoegd

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,14 +4,49 @@
   -->
 
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view />
+
+        <v-offline @detected-condition="amIOnline">
+            <template v-slot:[onlineSlot] :slot-name="onlineSlot" v-if="this.appOnline === false">
+                <div class="loading-overlay">
+                    <div class="d-block text-center m-3">
+                        <span class="spinner"></span>
+                        <br>
+                        <span class="text-info">Je hebt een online verbinding nodig.</span>
+                    </div>
+                </div>
+            </template>
+            <template v-slot:[offlineSlot] :slot-name="offlineSlot" v-if="this.appOnline === false">
+                <div class="loading-overlay">
+                    <div class="d-block text-center m-3">
+                        <span class="spinner"></span>
+                        <br>
+                        <span class="text-info">Je hebt een online verbinding nodig.</span>
+                    </div>
+                </div>
+            </template>
+        </v-offline>
+    </div>
 </template>
 
 <script>
+    import VOffline from 'v-offline';
 
-export default {
-  name: "App"
-};
+    export default {
+        name: "App",
+        components: {
+            VOffline
+        },
+        data: () => ({
+            appOnline: null,
+            onlineSlot: 'online',
+            offlineSlot: 'offline',
+        }),
+        methods: {
+            amIOnline(e) {
+                this.appOnline = e;
+            },
+        },
+    };
 </script>
