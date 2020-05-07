c5e44e6ab83cf132d3d18a2527ffa72be9aa0be5
Tygo Egmond <tygo.egmond@icloud.com>
2020-05-07 11:37:01 +0200

    [Client] Instellingen - Algemeen: Niet functioneel overlay toegevoegd

--- client/src/views/v1/gebruiker/instellingen/E-mailadres.vue
+++ client/src/views/v1/gebruiker/instellingen/E-mailadres.vue
@@ -5,20 +5,43 @@
 
 <template>
     <div class="panel">
-        <div class="panel-body">
-            <form action="">
-                <div class="form-group">
-                    <label for="email">E-mailadres</label>
-                    <h5>Nog niet mogelijk.</h5>
-                </div>
-            </form>
+        <form action="">
+            <div class="panel-body">
+                    <div class="form-group">
+                        <label for="email">E-mailadres</label>
+                        <input type="text" class="form-control" v-model="email">
+                    </div>
+            </div>
+            <div class="panel-footer">
+                <button type="submit" class="btn btn-contained btn-primary ml-auto" v-bind:disabled="this.laden">
+                    <span v-if="this.laden" class="spinner"></span>
+                    <span>E-mailadres veranderen</span>
+                </button>
+            </div>
+        </form>
+
+        <div class="loading-overlay">
+            <div class="d-block text-center m-3">
+                <span class="text-warning">Niet functioneel.</span>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+
     export default {
-        name: "E-mailadres.vue"
+        name: "E-mailadres.vue",
+        data() {
+            return {
+                email: ''
+            };
+        },
+        computed: mapGetters('auth', ['getGebruiker']),
+        mounted() {
+            this.email = this.getGebruiker.email;
+        }
     }
 </script>
 
