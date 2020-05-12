000e0e9969504b4a7fbe0d372801845a9d690af9
Tygo Egmond <tygo.egmond@icloud.com>
2020-05-12 12:06:59 +0200

    [Client] Auth header aangepast wat aansluit op het nieuwe Auth systeem

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,18 +1,18 @@
 /*
- * Copyright (c) 2020 
+ * Copyright (c) 2020
  * Dit project is gemaakt door Tygo Egmond (tygoegmond.nl)
  */
 
-import axios from 'axios';
+import axios from 'axios'
 
-const setAuthToken = token => {
+const setAuthToken = (token) => {
     if (token) {
-        localStorage.setItem('token', token);
-        axios.defaults.headers.common['x-auth-token'] = token;
+        localStorage.setItem('token', token)
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
     } else {
-        localStorage.removeItem('token');
-        delete axios.defaults.headers.common['x-auth-token'];
+        localStorage.removeItem('token')
+        delete axios.defaults.headers.common['x-auth-token']
     }
-};
+}
 
-export default setAuthToken;
+export default setAuthToken
