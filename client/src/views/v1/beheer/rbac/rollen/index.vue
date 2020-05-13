d139c2bc215b67bed6c786cca92306b7b7631fa4
Tygo Egmond <tygo.egmond@icloud.com>
2020-05-13 12:19:18 +0200

    [Client] Ophalen van alle rollen

--- client/src/views/v1/beheer/rbac/rollen/index.vue
+++ client/src/views/v1/beheer/rbac/rollen/index.vue
@@ -8,6 +8,8 @@
         <div class="container">
             <div class="row">
                 <div class="col">
+                    <div v-if="this.fout" class="notice notice-error my-4">{{ this.fout }}</div>
+
                     <div class="panel">
                         <div class="panel-header">
                             <span class="panel-title mr-auto">Rollen</span>
@@ -19,39 +21,48 @@
                                     <tr>
                                         <th style="min-width: auto; width: auto;">
                                             <div class="column-content flex-row align-items-center justify-content-start">
+                                                <span>Weergavenaam</span>
+                                            </div>
+                                        </th>
+                                        <th style="min-width: auto; width: auto;">
+                                            <div class="column-content flex-row align-items-center justify-content-start">
                                                 <span>Systeemnaam</span>
                                             </div>
                                         </th>
-
                                         <th style="min-width: auto; width: auto;">
                                             <div class="column-content flex-row align-items-center justify-content-start">
-                                                <span>Gebruikers</span>
+                                                <span>Beschrijving</span>
                                             </div>
                                         </th>
+
+                                        <th style="min-width: 150px; width: auto;"></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-<!--                                    <tr>-->
-<!--                                        <td style="width: auto;">-->
-<!--                                            <div class="column-content">-->
-<!--                                                Systeembeheerder-->
-<!--                                            </div>-->
-<!--                                        </td>-->
-
-<!--                                        <td style="width: auto;">-->
-<!--                                            <div class="column-content">-->
-<!--                                                1-->
-<!--                                            </div>-->
-<!--                                        </td>-->
-
-<!--                                        <td style="width: auto; max-width: 80px">-->
-<!--                                            <div class="column-content">-->
-<!--                                                <a href="" class="btn btn-outline btn-primary">-->
-<!--                                                    <span>Meer info</span>-->
-<!--                                                </a>-->
-<!--                                            </div>-->
-<!--                                        </td>-->
-<!--                                    </tr>-->
+                                    <tr v-for="rol in rollen" :key="rol.id">
+                                        <td style="width: auto;">
+                                            <div class="column-content">
+                                                {{ rol.display_name }}
+                                            </div>
+                                        </td>
+                                        <td style="width: auto;">
+                                            <div class="column-content">
+                                                {{ rol.name }}
+                                            </div>
+                                        </td>
+                                        <td style="width: auto;">
+                                            <div class="column-content">
+                                                {{ rol.description || 'Geen beschrijving.' }}
+                                            </div>
+                                        </td>
+                                        <td style="width: auto;">
+                                            <div class="column-content">
+                                                <router-link class="btn btn-outline btn-primary" :to="{ name: 'RBACRollenView', params: { rol_id: rol.id } }">
+                                                    <span>Meer info</span>
+                                                </router-link>
+                                            </div>
+                                        </td>
+                                    </tr>
                                 </tbody>
 
                                 <tfoot>
@@ -66,10 +77,8 @@
                             </table>
                         </div>
 
-                        <div class="loading-overlay">
-                            <div class="d-block text-center m-3">
-                                <span class="text-warning">Niet functioneel.</span>
-                            </div>
+                        <div class="loading-overlay" v-if="this.laden">
+                            <span class="spinner"></span>
                         </div>
                     </div>
                 </div>
@@ -79,7 +88,41 @@
 </template>
 
 <script>
+    import axios from "axios";
+    import {Latte} from "@bybas/latte-ui";
+
     export default {
-        name: "rollen-index"
+        name: "rollen-index",
+
+        data() {
+            return {
+                fout: '',
+                laden: true,
+                rollen: []
+            };
+        },
+
+        methods: {
+            async getRollen() {
+                if (!this.laden) {
+                    return;
+                }
+
+                try {
+                    const res = await axios.get(process.env.VUE_APP_APIURL + '/api/v1/beheer/rbac/rollen');
+                    this.rollen = res.data.rollen;
+                    this.laden = false;
+                } catch (err) {
+                    await Latte.ui.notification.create({
+                        title: 'Er is iets fout gegaan!',
+                        message: 'Probeer het later opnieuw.'
+                    });
+                }
+            },
+        },
+
+        created() {
+            this.getRollen();
+        }
     }
 </script>
